36fc1dce729a6297f1f4c719c09e73d5caeab8f9
Nguyễn Việt Tiệp <viettiep0412@gmail.com>
2020-07-04 12:39:05 +0700

    Add project files.

---
+++ 1. Overview/Page1.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfWebStyle
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+
+
+
+        //private void Buttonclick(object sender, RoutedEventArgs e) Click
+        //{
+        //    MessageBox.Show("this function is activated");
+        //}
+
+        //private void Buttonclick(object sender, MouseEventArgs e) Mouse enter
+        //{
+        //    MessageBox.Show("this function is activated");
+        //}
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfWebStyle
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent(); 
+            Tracing.Enable();
+        }
+
+        private void Quit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //private void Test1(object sender, System.Windows.Input.MouseButtonEventArgs e) ok mousedoubleclick
+        //{
+          
+        //}
+
+
+        //private void Btn_Gotfocus(object sender, RoutedEventArgs e)
+        //{
+        //    frame1.NavigationService.Navigate(new Page1());
+        //}
+
+
+
+        //private void Test1(object sender, RoutedEventArgs e) loaded
+        //{
+        //    MessageBox.Show("Menu item clicked");
+        //}
+
+        //private void Test1(object sender, DependencyPropertyChangedEventArgs e) Ismousecapturechanged
+        //{
+        //    MessageBox.Show("Menu item clicked");
+        //}
+
+        //private void Test1(object sender, DependencyPropertyChangedEventArgs e) Isenablechanged
+        //{
+        //    MessageBox.Show("Menu item clicked");
+        //}
+
+        //private void Test1(object sender, System.EventArgs e) Item initialized
+        //{
+        //    MessageBox.Show("Menu item clicked");
+        //}
+
+    }
+}
